b3bd56a608e6be66cccaa7ab544399dc364e6507
Bence Juhos <ben@fuzzylogicstudio.io>
2021-01-15 07:50:31 +0100

    ui indicator for game state changing

---
+++ Assets/Scripts/UI/GUI/GameStatePanel.cs
@@ -0,0 +1,37 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStatePanel : MonoBehaviour
+{
+    public float SlideTime = 1f;
+    public RectTransform Slider;
+    public Transform ItemContainer;
+    public GameStatePanelItem[] Items;
+    public Image CurrentItemImage;
+
+    private void Start()
+    {
+        Close();
+    }
+
+    public void Open()
+    {
+        Slider.DOAnchorPos(Vector2.zero, SlideTime);
+    }
+
+    public void Close()
+    {
+        Slider.DOAnchorPos(Vector2.right * Slider.rect.width, SlideTime);
+    }
+
+    public void SelectItem(GameState state)
+    {
+        CurrentItemImage.sprite = state.Icon;
+
+        foreach (var item in Items)
+            item.gameObject.SetActive(item.State != state);
+    }
+}
---
+++ Assets/Scripts/UI/GUI/GameStatePanelItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameStatePanelItem : MonoBehaviour
+{
+    public GameState State;
+    public Image Icon;
+
+    private void Start()
+    {
+        Icon.sprite = State.Icon;
+    }
+}
